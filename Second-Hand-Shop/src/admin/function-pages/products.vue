399b63735542e45ece4f929d4a16c498d97d0231
tin.chen <s1101726@gm.pu.edu.tw>
2024-05-18 22:47:32 +0800

    新增 後台管理上架商品

--- Second-Hand-Shop/src/admin/function-pages/products.vue
+++ Second-Hand-Shop/src/admin/function-pages/products.vue
@@ -1,33 +1,65 @@
 <script setup>
-    import adminHeader from '@/admin/admin-header.vue'
-    import adminSidebar from '@/admin/admin-sidebar.vue'
+import adminHeader from '@/admin/admin-header.vue'
+import adminSidebar from '@/admin/admin-sidebar.vue'
+
 </script>
 
 <script>
-    import axios from 'axios';
-    export default {
-        name: 'adminProducts',
-        data(){
-            return {
-                products:[]
-            };
-        },
-        created(){
-            this.fetchProducts();
-        },
-        methods :{
-            async fetchProducts(){
-                await axios.get('http://localhost:3000/products/')
-                .then(respones=>{
-                    this.products= respones.data;
-                    console.log (this.products);
-                })
-                .catch(error=>{
-                    console.error ("There was an error fetching the users!", error);
-                })
+import axios from 'axios';
+export default {
+    name: 'adminProducts',
+    data() {
+        return {
+            products: [],
+            newProduct: {
+                category: '',
+                name: '',
+                productCode: '',
+                imageUrl: '',
+                description: '',
+                price: ''
             }
+        };
+    },
+    created() {
+        this.fetchProducts();
+    },
+    methods: {
+        async fetchProducts() {
+            try {
+                const response = await axios.get('http://localhost:3001/products/');
+                this.products = response.data;
+                console.log(this.products);
+            } catch (error) {
+                console.error("There was an error fetching the products!", error);
+            }
+        },
+        async addProduct() {
+            try {
+                const response = await axios.post('http://localhost:3000/products/', this.newProduct);
+                if (response.status === 200 || response.status === 201) {
+                    this.fetchProducts();
+                    alert('Product added successfully');
+                    this.resetForm();
+                    
+                }
+            } catch (error) {
+                console.error("There was an error adding the product!", error);
+                alert('Error adding product');
+            }
+        },
+        resetForm() {
+            this.newProduct = {
+                category: '',
+                name: '',
+                productCode: '',
+                imageUrl: '',
+                description: '',
+                price: ''
+            };
         }
     }
+}
 </script>
 
 <template>
@@ -36,6 +68,7 @@
     <div class="container">
         <div class="d-flex justify-content-between mt-2">
             <h3>商品管理列表 Products Management</h3>
+            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#login-modal">建立新商品</button>
         </div>
         <hr>
         <table class="table mt-4 table-bordered">
@@ -49,25 +82,68 @@
             </thead>
             <tbody>
                 <tr v-for="product in products" :key="product.id">
-                    <td class="p-3">{{ product.catelog }}</td>
-                    <td class="p-3">{{ product.product_name }}/{{ product.number }}</td>
-                    <td class="p-3">{{ product.price}}</td>
+                    <td class="p-3">{{ product.category }}</td>
+                    <td class="p-3">{{ product.product_name }}/{{ product.product_code }}</td>
+                    <td class="p-3">{{ product.price }}</td>
                     <td>
                         <div class="d-flex justify-content-center">
                             <button class="btn btn-outline-primary btn-sm mx-3">編輯</button>
                             <button class="btn btn-outline-danger btn-sm mx-3">刪除</button>
                         </div>
                     </td>
-
                 </tr>
             </tbody>
         </table>
     </div>
+    <!-- modal -->
+    <div id="login-modal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!--Header-->
+                <div class="modal-header">
+                    <div class="modal-container text-center">
+                        <h3><i class="bi bi-person-circle me-1"></i>商品上架</h3>
+                    </div>
+                    <button class="btn-close" type="button" data-bs-dismiss="modal"></button>
+                </div>
+                <!--Body-->
+                <div class="modal-body">
+                    <form @submit.prevent="addProduct">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.category" placeholder="輸入分類項目">
+                            <label for="floatingInput">輸入分類項目</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.name" placeholder="輸入商品名稱">
+                            <label for="floatingInput">輸入商品名稱</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.productCode" placeholder="輸入商品編號">
+                            <label for="floatingInput">輸入商品編號</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.imageUrl" placeholder="輸入圖片網址">
+                            <label for="floatingInput">輸入圖片網址</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.description" placeholder="輸入產品描述">
+                            <label for="floatingInput">輸入產品描述</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control mb-3" v-model="newProduct.price" placeholder="輸入價格">
+                            <label for="floatingInput">輸入價格</label>
+                        </div>
+                        <button type="submit" class="btn btn-outline-success w-100">送出</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
-    .container {
-        padding-top: 48px;
-        padding-left: 250px;
-    }
+.container {
+    padding-top: 48px;
+    padding-left: 250px;
+}
 </style>
--- Second-Hand-Shop/src/website_homepage/main_content.vue
+++ Second-Hand-Shop/src/website_homepage/main_content.vue
@@ -1,5 +1,11 @@
 <script setup>
-
+const cards = [
+  { text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', time: '9 mins' },
+  { text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', time: '10 mins' },
+  { text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', time: '9 mins' },
+  { text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', time: '9 mins' },
+  // Add more card data as needed
+];
 </script>
 
 <template>
@@ -55,158 +61,30 @@
 </div>
 
 <div class="album py-5 bg-light">
-  <div class="container">
-
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div v-for="(card, index) in cards" :key="index" class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#55595c"></rect>
+                <text x="50%" y="50%" fill="#eceeef" dy=".3em">照片</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">{{ card.text }}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">檢視</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">收藏</button>
+                  </div>
+                  <small class="text-muted">{{ card.time }}</small>
+                </div>
               </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">10 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-          <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-              </div>
-              <small class="text-muted">9 mins</small>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
 
 </main>
 
